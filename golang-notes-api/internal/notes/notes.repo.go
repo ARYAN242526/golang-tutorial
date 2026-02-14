d47e861b86a9e79c841211c8338e343ec26744f4
ARYAN242526 <roy2503aryan@gmail.com>
2026-02-14 15:21:44 +0530

    feat : list notes endpoint added

--- golang-notes-api/internal/notes/notes.repo.go
+++ golang-notes-api/internal/notes/notes.repo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -32,4 +33,30 @@ func (r *Repo) Create(ctx context.Context , note Note) (Note, error){
 	}
 
 	return note , nil
+}
+
+func (r *Repo) List(ctx context.Context) ([]Note , error) {
+
+	opCtx , cancel := context.WithTimeout(ctx , 5 * time.Second)
+	defer cancel()
+
+	filter := bson.M{} // match all docs
+
+	// Find returns a cursor (like an iterator)
+	// over matching documents
+	cursor , err := r.coll.Find(opCtx , filter)
+	if err != nil {
+		return nil , fmt.Errorf("Find noters failed: %w" , err)
+	}
+
+	// cursor must be closed after use
+	defer cursor.Close(opCtx)
+
+	var notes []Note
+
+	if err := cursor.All(opCtx , &notes); err != nil {
+		return nil , fmt.Errorf("Decode notes failed: %w" , err)
+	}
+
+	return notes , nil
 }
